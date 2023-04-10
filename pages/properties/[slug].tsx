36ac54b777ec111a063cf00aa62bfa2c79812b6c
kevinshen56714 <kshen64@gatech.edu>
2023-04-09 23:59:18 -0400

    Add NextJS and Supabase

---
+++ pages/properties/[slug].tsx
@@ -0,0 +1,65 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+
+import { ParsedUrlQuery } from "querystring";
+import { supabase } from "./../../lib/supabaseClient";
+
+interface Params extends ParsedUrlQuery {
+  slug: string;
+}
+
+const properties = {
+  Tg: "Glass Transition Temperature",
+  gas_diffusivity: "Glass Diffusivity",
+  gas_solubility: "Gas Solubility",
+  solvent_diffusivity: "Solvent Diffusivity",
+};
+
+function Page(props: {
+  property: {
+    smiles: string;
+    value: number;
+    gas?: string; // for gas diffusivity and solubility
+    solvent_smiles?: string; // for solvent diffusivity
+    ratio?: number; // for solvent diffusivity
+  }[];
+}) {
+  return (
+    <ul>
+      {props.property.map((entry) => (
+        <li key={entry.smiles}>
+          {entry.smiles}
+          {entry.value}
+          {entry.gas ? entry.gas : ""}
+          {entry.solvent_smiles ? entry.solvent_smiles : ""}
+          {entry.ratio ? entry.ratio : ""}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// This function gets called at build time
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Get the paths we want to pre-render based on cities and times
+  const paths = Object.keys(properties).map((slug) => ({
+    params: { slug },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+};
+
+// This also gets called at build time
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { slug } = context.params as Params;
+  let { data } = await supabase.from(slug).select();
+
+  return {
+    props: {
+      property: data,
+    },
+  };
+};
+
+export default Page;
